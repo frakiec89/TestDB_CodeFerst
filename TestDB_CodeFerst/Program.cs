d45e02793775aa40f8a83dd487a9b667cea411c9
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-03-02 14:45:36 +0400

    Добавьте файлы проекта.

---
+++ TestDB_CodeFerst/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestDB_CodeFerst
+{
+    internal class Program // это view
+    {
+        static string getRead = "get"; // команды
+        static string addRead = "add";
+        static string exit = "exit";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа -- мои  записки --- ");
+
+            while (true)
+            {
+                Console.WriteLine($"для того что  бы  получит  список  записей введите \"{getRead}\"");
+                Console.WriteLine($"для того что  бы  добавить  запись введите \"{addRead}\"");
+                Console.WriteLine($"для того что  бы  выйти из программы введите \"{exit}\"");
+
+                switch (Console.ReadLine().ToLower().TrimStart().TrimEnd())
+                {   
+                    case "get":  GetRead() ; break;
+                    case "add":  AddRead(); break;
+                    case "exit":; return;
+                    default:  Console.WriteLine("не корректная команда");
+                        break;
+                }
+            }
+        }
+
+        private static void AddRead()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GetRead()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
