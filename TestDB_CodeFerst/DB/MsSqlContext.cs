ae57f6932ca247825c13b9999c92984f7585b6fb
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-03-02 16:49:53 +0400

    dsdsd

---
+++ TestDB_CodeFerst/DB/MsSqlContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace TestDB_CodeFerst.DB
+{
+    public class MsSqlContext :DbContext
+    {
+        static string adress = "192.168.10.134";
+        static string name = "AgtyamovTestDb_IS_20_03"; //так будет  называться бд - у каждого своя
+        static string login = "stud";
+        static string password = "stud";
+
+        string connectionString = $"Server = {adress}; Database = {name}; User Id = {login}; Password = {password};";
+        
+
+     
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Record> Records { get; set; }
+    }
+}
---
+++ TestDB_CodeFerst/DB/Record.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestDB_CodeFerst.DB
+{
+    public class Record
+    {
+        public int RecordId { get; set; }
+        public string Content { get; set; } 
+        public  string Status { get; set; }
+        public DateTime DateRecord { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
+
+
